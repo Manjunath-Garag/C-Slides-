c9e30367e75ed689f806d30b4b7a226c1c0d53c0
Manjunath-Garag <32258833+Manjunath-Garag@users.noreply.github.com>
2017-11-30 20:12:25 +0530

    Create array1.c

---
+++ ecep/advanceC/basicC/array1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+int x; //single variable.
+int y[5]; // bunch of five varables
+
+x=2;
+
+y[0]=10;  // initilaization of singlr var in the bunch of 5 variables.
+y[1]=20;
+y[2]=30;
+y[3]=40;
+y[4]=50;
+
+printf("x=%d, y[0]= %d\n",x,y[0]);
+
+return 0;
+}
